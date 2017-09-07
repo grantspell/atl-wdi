9c944e0eccae19109537e19934705d79030060f5
grantspell <spellgrant@gmail.com>
2017-09-07 16:57:59 -0400

    end_of_thursday

--- labs/javaScript/in-class-click-events/script.js
+++ labs/javaScript/in-class-click-events/script.js
@@ -1,2 +1,15 @@
 window.onload = function() {
+   const buttonOne = document.querySelector(".buttonOne")
+    buttonOne.addEventListener("click", function(){
+        alert("you have clicked button one")
+    });
+    const buttonTwo = document.querySelector(".buttonTwo")
+    buttonTwo.addEventListener("click", function(){
+        const paragraph = document.getElementById('information');
+        paragraph.innerText = "A click event is essentially tying a function (as a callback) to an element and specifying what action needs to happen to trigger or call/invoke that function (callback)."; 
+    });
+    const buttonThree = document.querySelector(".buttonThree")
+    buttonThree.addEventListener("click", function(){
+        document.getElementsByTagName("div")[1].innerHTML = "";
+    });
 }
--- morning_exercises/pseudocode/script.js
+++ morning_exercises/pseudocode/script.js
@@ -3,23 +3,47 @@
 // Barrels O' RUM
 
 var barrels = function(small, large, total){
+  let smallB = 60;
+  let largeB = (total - (small * smallB)) / large;
+
+  return largeB;
+
 };
+
+barrels(2, 3, 825);
+//=> '235'
 
 //*************************
 // Problem 2:
 // Sailing the Seas
 
 var shipFuelCost = function(fuelPrice, milesPerGallon){
-  //your code here
+  let earthC = 24901;
+  let galUsed = earthC / milesPerGallon;
+  let total = galUsed * fuelPrice;
+
+  return total;
 };
+
+shipFuelCost(3, 12);
+//=> '6225.25'
 
 //*************************
 // Problem 3:
 // GROG
 
-var calcFruitJuice = function(a, b, c, d){
-  //your code here
+var calcFruitJuice = function(costcoGal, costcoPerc, kirkGal, kirkPerc){
+  let costcoT = (costcoPerc / 100) * costcoGal;
+  let kirkT = (kirkPerc / 100) * kirkGal;
+  let grandPercT = costcoT + kirkT;
+  let galT = costcoGal + kirkGal;
+  let finalPerc = (grandPercT) / (galT);
+  
+  return finalPerc;
 };
+
+calcFruitJuice(3, 20, 2, 55);
+//=> '0.34'
 
 //DO NOT EDIT BELOW THIS LINE//
 module.exports = {
@@ -27,3 +51,5 @@ module.exports = {
   shipFuelCost: shipFuelCost,
   calcFruitJuice: calcFruitJuice
 };
+
+
