4940295bf40100e3fb500a206e9fbb31d6564cf7
grantspell <spellgrant@gmail.com>
2017-09-20 09:53:58 -0400

    Wednesday Morning

--- homework/pizza-express/app.js
+++ homework/pizza-express/app.js
@@ -4,6 +4,10 @@ const express = require('express');
 const app = express();
 //assigning 3000 as our port
 const PORT = 3000;
+const hbs = require('hbs');
+
+app.set("view engine", "hbs");
+app.set('views', './views');
 
 //tells the server to listen for requests on port 3000
 app.listen(PORT, function() {
@@ -11,3 +15,15 @@ app.listen(PORT, function() {
     console.log('LISTENING ON PORT ' + PORT);
     console.log("==========================")
 });
+
+app.get("/", (req, res) => {
+    const message = "Welcome to Pizza Express!"
+
+    res.send( message )
+})
+
+app.get("/topping/:type", function(req, res, next) {
+    let type = 
+
+    res.send()
+})
