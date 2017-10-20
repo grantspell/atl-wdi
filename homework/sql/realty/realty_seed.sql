31671bf079b26220c40557273597142cc0826a90
grantspell <spellgrant@gmail.com>
2017-10-20 10:50:48 -0400

    PUSH

---
+++ homework/sql/realty/realty_seed.sql
@@ -0,0 +1,14 @@
+INSERT INTO apartments (apt_num, bed_num, bath_num, address, tenant, vacant, sq_foot, price) VALUES (007, 2, 2, '888 N Dr, Atlanta, GA, 30318','Jefferey Swan', false, 1400, 150000);
+INSERT INTO apartments (apt_num, bed_num, bath_num, address, tenant, vacant, sq_foot, price) VALUES (101, 1, 6, '9 Oak Harvest Whhhhhood Lane, Upstate', 'Gervais Garns', true, 999, 13000);
+INSERT INTO apartments (apt_num, bed_num, bath_num, address, tenant, vacant, sq_foot, price) VALUES (177, 4, 5, '1800 Your Home, Atlanta, GA', 'Kassidy Rodgers', false, 3500, 2000000);
+INSERT INTO apartments (apt_num, bed_num, bath_num, address, tenant, vacant, sq_foot, price) VALUES (666, 6, 6, '6th St, Downtown', 'Lucy', true, 6066, 363636);
+
+INSERT INTO offices (off_num, floor_num, sq_foot, cub_num, bath_num, address, company_name, vacant, price) VALUES (890, 3, 1100, 2, 2, 'Donahue Building, 11th St, Downtown', 'Sherns, Inc.', true, 80000);
+INSERT INTO offices (off_num, floor_num, sq_foot, cub_num, bath_num, address, company_name, vacant, price) VALUES (013, 2, 1450, 4, 2, '90 Run Dog Drive, Decatur, GA', 'YouCanToo! Startups', false, 0);
+INSERT INTO offices (off_num, floor_num, sq_foot, cub_num, bath_num, address, company_name, vacant, price) VALUES (003, 12, 900, 1, 0, '8th South Lane, Atlanta, GA', 'Bonjour Co.', true, 10000);
+INSERT INTO offices (off_num, floor_num, sq_foot, cub_num, bath_num, address, company_name, vacant, price) VALUES (077, 27, 20000, 12, 4, 'Price Building, Downtown', 'Stargazer Ent.', false, 2000000);
+
+INSERT INTO storefronts (address, vacant, price, kitchen, sq_foot, owner, patio) VALUES ('888 N Dr, Atlanta, GA, 30318', false, 10000, true, 1300, 'Bob Dylan', true);
+INSERT INTO storefronts (address, vacant, price, kitchen, sq_foot, owner, patio) VALUES ('Donahue Building, 11th St, Downtown', true, 13000, false, 1000, 'Kevin Spacey', false);
+INSERT INTO storefronts (address, vacant, price, kitchen, sq_foot, owner, patio) VALUES ('Donahue Building, 11th St, Downtown', true, 16000, true, 1200, 'Kevin Spacey', true);
+INSERT INTO storefronts (address, vacant, price, kitchen, sq_foot, owner, patio) VALUES ('Donahue Building, 11th St, Downtown', true, 16000, true, 1200, 'Kevin Spacey', true);
