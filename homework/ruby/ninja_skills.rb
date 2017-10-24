2afed8caa050eece6696052066b25c352c2951f8
grantspell <spellgrant@gmail.com>
2017-10-23 23:36:16 -0400

    2. Nil skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -1,20 +1,28 @@
-# CHALLENGE 1: BOOLEANS
+# # CHALLENGE 1: BOOLEANS
 
-## 1 | Create a variable `a` and set it equal to true
+# ## 1 | Create a variable `a` and set it equal to true
 
- a = true
+#  a = true
 
-## 2 | Create a variable `b` and set it equal to false
+# ## 2 | Create a variable `b` and set it equal to false
 
- b = false
+#  b = false
 
-## 3 | Write a statement using `a` and `b` that evaluates to false [puts the result to the terminal]
+# ## 3 | Write a statement using `a` and `b` that evaluates to false [puts the result to the terminal]
 
-puts a == b
+# puts a == b
 
-## 4 | Write a statment using `a` and `b` that evaluates to `true` [puts the result to the terminal]
+# ## 4 | Write a statment using `a` and `b` that evaluates to `true` [puts the result to the terminal]
 
-puts b = a
+# puts b = a
 
-## 5 | Commit -m "1. Boolean Skills"
+# ## 5 | Commit -m "1. Boolean Skills"
 
+# Challenge 2: Nil
+
+a_simple_variable = nil
+
+p a_simple_variable
+puts "#{ a_simple_variable } was here!"
+print a_simple_variable
+puts a_simple_variable.class
